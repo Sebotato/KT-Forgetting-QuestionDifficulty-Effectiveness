b541616c475919d39c4b0c4039eb82b332cede2c
Zhenhui Gu <107767605+Sebotato@users.noreply.github.com>
2024-09-15 22:20:59 +1000

    Initial commit

---
+++ edustudio/traintpl/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author : Xiangzhi Chen
+# @Github : kervias
+
+from .base_traintpl import BaseTrainTPL
+from .gd_traintpl import GDTrainTPL
+from .general_traintpl import GeneralTrainTPL
